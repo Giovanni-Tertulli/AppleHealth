9c8cd9bf9a76846cb5fc3c0bc6ff91c5e57617fd
Giovanni Tertulli <70154428+Giovanni-Tertulli@users.noreply.github.com>
2022-08-16 15:23:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 # setup.py placed at root directory
-from setuptools import setup
+from setuptools import setup, find_packages
+import codecs
+import os
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Streaming video data via networks'
+
+# Setting up
 setup(
-    name="activity_summary",
-    version='1.0.0',
-    author='Giovanni Angelo Tertulli',
-    description='This is an example project',
-    long_description='This is a longer description for the project',
-    keywords='sample, example, setuptools',
-    python_requires='>=3.7, <4',
-    install_requires=[
-        'PyYAML',
-        'pandas==0.23.3',
-        'numpy>=1.14.5',
-        'matplotlib>=2.2.0',
-        'jupyter'
-    ],
-    install_requires=["activity_summary"],
-    extras_require={
-        'test': ['pytest', 'coverage'],
-    },
-    package_data={
-        'sample': ['ActivitySummary.csv'],
-    },
-    entry_points={
-        'runners': [
-            'sample=sample:main',
-        ]
-    }
+    name="health_processor",
+    version=VERSION,
+    author="Giovanni Angelo Tertulli",
+    author_email="<giovanni.tertulli@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['],
+    keywords=['python', 'Apple', 'Health],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
